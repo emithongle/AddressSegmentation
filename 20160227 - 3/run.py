b44cc95e3829f96c07db833f38baffcbb44d20d7
ThongLe <thong.le@emichat.com>
2016-02-27 23:00:47 +0700

    Expanding to test 3-layer NN models.

---
+++ 20160227 - 3/run.py
@@ -0,0 +1,34 @@
+__author__ = 'Thong_Le'
+
+from libs.features import extractFeature, randomSample
+from libs.models import buildClassifer
+from libs.config import *
+
+# 1. Read txt data
+tupleData = store.loadTxtData()
+
+# 2. Extract features
+featureTuples = extractFeature(feature_func, preprocessing_func, tupleData)
+store.saveFeature(featureTuples)
+
+# 3.1 Random Training and Testing Data
+x = randomSample()
+store.saveTrainingTestingData(x)
+
+# 3.2 Load Training and Testing Data
+modelData = store.loadTrainingTestingData()
+
+# 3.3 Train model
+print('=> Training model...')
+clf_model = buildClassifer()
+clf_model.fit(modelData['X_train'], modelData['y_train'])
+
+y_hat = clf_model.predict(modelData['X_test'])
+print('Acc = ', sum([1 for (y1, y2) in zip(modelData['y_test'], y_hat) if (y1 == y2)]) / len(y_hat))
+
+store.saveClassifier(clf_model)
+
+# 4. Test
+import test
+
+None
