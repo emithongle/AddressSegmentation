cd92875f36f47fb8b3a90909022e269a5579aa64
ThongLe <thong.le@emichat.com>
2016-03-14 13:35:40 +0700

    Update testing feature %digits.

--- 20160311 - 001/execute/test_address_segment.py
+++ 20160311 - 001/execute/test_address_segment.py
@@ -7,9 +7,15 @@ from config import *
 def exc(file='testdata.txt'):
     # 5. Test
     textList = store.loadTextData(file=file)
-    _time, templateList = sg.parseAddress(textList)
+    if (file_model):
+        _time, templateList = sg.parseAddress(textList, file_model)
+    else:
+        _time, templateList = sg.parseAddress(textList)
 
     titles = [['#', 'Text', 'Runtime'],
               ['TestCase', 'Top', 'Name', 'Address', 'Phone', 'NameScore', 'AddressScore', 'PhoneScore', 'Score']]
 
-    store.saveResults(titles, (textList, _time), templateList, file=timeManage.getTime() + '_' + file_segment_address_result)
+    if (file_model):
+        store.saveResults(titles, (textList, _time), templateList, file=file_model + '_' + file_segment_address_result)
+    else:
+        store.saveResults(titles, (textList, _time), templateList, file=timeManage.getTime() + '_' + file_segment_address_result)
--- 20160311 - 001/libs/features.py
+++ 20160311 - 001/libs/features.py
@@ -165,8 +165,16 @@ def feature(text):
     tc_0, td_0 = findMaxString(text, 0)
 
     return [
-             sum([1 for c in text if (c in string.digits)]) / textLen
-            ]
+             sum([1 for c in text if (c in string.digits)]) / textLen,
+             len(td_0) / textLen,
+             sum([1 for c in text if (c in string.ascii_letters)]) / textLen,
+             len(text)
+            ] + \
+            ([
+                # First Character Type
+                 1 if checkCharacterType(text[0])[True] == 'ascii' else 0,
+                 1 if checkCharacterType(text[0])[True] == 'digit' else 0
+            ] if len(text) >= 1 else [0, 0])
 
 def dataPreprocess(tupleData):
 
