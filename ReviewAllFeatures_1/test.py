376c688f8dad9309f035f89a4938407ef935b33f
ThongLe <thong.le@emichat.com>
2016-03-22 15:45:56 +0700

    Test all features: length, #ascii, #digit, %ascii, %digits, %kwName, %kwAddress, %kwPhone, %max_digit_skip_0, first_character_ascii, first_character_digit, last_character_ascii', last_character_digit.

---
+++ ReviewAllFeatures_1/test.py
@@ -0,0 +1,17 @@
+
+from libs.segment import templateSegment
+
+# X = templateSegment('81 Duong 16, P. Binh Tri Dong B, Q.Binh Tan, 0909218877, Dinh Thi Bich Phuong', 3)
+
+# X = templateSegment('a,cb         b', 3)
+
+# from libs.features import feature
+#
+# X = feature('nguyen thi thanh thuy')
+#
+# None
+
+from execute.test_address_segment import exc
+
+exc()
+None
